ffae44de61439ad10b79028fd7b2b47e376b511b
Mauricio Rojas <mrojasb2000@gmail.com>
2024-08-12 18:49:31 -0400

    initial commit

---
+++ src/main/java/com/bram/car_database/CarDatabaseApplication.java
@@ -0,0 +1,16 @@
+package com.bram.car_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@SpringBootApplication
+public class CarDatabaseApplication {
+	private static final Logger logger = LoggerFactory.getLogger(CarDatabaseApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(CarDatabaseApplication.class, args);
+		logger.info("Application Started");
+	}
+}
---
+++ src/test/java/com/bram/car_database/CarDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bram.car_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
